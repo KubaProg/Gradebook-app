16394016212d063ab875f49290f87af0e11a2216
Jakub Opiełka <jakub1.opielka1@interia.pl>
2023-05-18 15:53:57 +0200

    Deleted security dependency temporary + fundamental entities created

--- src/main/java/pl/polsl/gradebook/Teacher/Teacher.java
+++ src/main/java/pl/polsl/gradebook/Teacher/Teacher.java
@@ -1,7 +1,23 @@
 package pl.polsl.gradebook.Teacher;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Teacher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
 }
---
+++ src/main/java/pl/polsl/gradebook/Web/HomeController.java
@@ -0,0 +1,15 @@
+package pl.polsl.gradebook.Web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String hello(){
+        return "landingPage";
+    }
+
+
+}
