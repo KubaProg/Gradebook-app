0f0e98b5a231ee0999822c03747dd4472685a1e6
Dominik Szczepanik <dominik16333@gmail.com>
2023-06-08 17:34:12 +0200

    adding users to database

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,16 @@
-INSERT INTO users (login, password, role)
-VALUES ('jakub', '{bcrypt}$2a$12$YyPMqSXuWWX7nLzzyIW6O.Uj32KJO4h4VHK6YDcB00WmXLSpaAVnK', 'USER'),
-    ('dominik', '{bcrypt}$2a$12$Z229lO.J4rcdoNKwKJiCDevl2hcv5Mf6FxxQvBL5knpKtBNZ0Z6le', 'USER'),
-    ('michal', '{bcrypt}$2a$12$komupxf.cy31oi.HLvUQKexyXH4N7Mbz6TCRe5nlHTI5HSHtmsxae', 'ADMIN');
+INSERT INTO users (id,login, password, role)
+VALUES (1,'jakub', '{bcrypt}$2a$12$YyPMqSXuWWX7nLzzyIW6O.Uj32KJO4h4VHK6YDcB00WmXLSpaAVnK', 'STUDENT'),
+    (2,'dominik', '{bcrypt}$2a$12$Z229lO.J4rcdoNKwKJiCDevl2hcv5Mf6FxxQvBL5knpKtBNZ0Z6le', 'TEACHER'),
+    (3,'michal', '{bcrypt}$2a$12$komupxf.cy31oi.HLvUQKexyXH4N7Mbz6TCRe5nlHTI5HSHtmsxae', 'HEADMASTER');
+COMMIT;
 
+
+
+INSERT INTO student (id,name, parent_number, surname, user_id)
+VALUES (1,'jakub','123456','opielka',1);
+
+INSERT INTO teacher (id,name,salary, surname, user_id)
+VALUES (2,'dominik',4300,'szczepanik',2);
+
+INSERT INTO headmaster (id,name, surname, user_id)
+VALUES (3, 'michal','moryc',3);
