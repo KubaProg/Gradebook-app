ba6a4eff1a15551ffa1ba471452ba5eaffeb362c
Jakub Opiełka <jakub1.opielka1@interia.pl>
2023-06-15 12:25:58 +0200

    Old sql file retrival

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,7 @@
 INSERT INTO users (id,login, password, role)
 VALUES (1,'jakub', '{bcrypt}$2a$12$YyPMqSXuWWX7nLzzyIW6O.Uj32KJO4h4VHK6YDcB00WmXLSpaAVnK', 'STUDENT'),
-    (2,'dominik', '{bcrypt}$2a$12$Z229lO.J4rcdoNKwKJiCDevl2hcv5Mf6FxxQvBL5knpKtBNZ0Z6le', 'TEACHER'),
-    (3,'michal', '{bcrypt}$2a$12$komupxf.cy31oi.HLvUQKexyXH4N7Mbz6TCRe5nlHTI5HSHtmsxae', 'HEADMASTER'),
+       (2,'dominik', '{bcrypt}$2a$12$Z229lO.J4rcdoNKwKJiCDevl2hcv5Mf6FxxQvBL5knpKtBNZ0Z6le', 'TEACHER'),
+       (3,'michal', '{bcrypt}$2a$12$komupxf.cy31oi.HLvUQKexyXH4N7Mbz6TCRe5nlHTI5HSHtmsxae', 'HEADMASTER'),
        (4,'arek', '{bcrypt}$2a$12$komupxf.cy31oi.HLvUQKexyXH4N7Mbz6TCRe5nlHTI5HSHtmsxae', 'STUDENT');
 COMMIT;
 
@@ -11,18 +11,18 @@ VALUES (1,'jakub','123456','opielka',1),
        (2,'arek','567890','Banasik',4);
 
 INSERT INTO teacher (id,name,salary, surname, user_id)
-VALUES (1,'dominik',4300,'szczepanik',2);
+VALUES (2,'dominik',4300,'szczepanik',2);
 
 INSERT INTO headmaster (id,name, surname, user_id)
-VALUES (1, 'michal','moryc',3);
+VALUES (3, 'michal','moryc',3);
 
 INSERT INTO Subject (teacher_id, name)
-VALUES (1, 'Mathematics') , (1, 'Physics'), (1, 'English');
+VALUES (2, 'Mathematics') , (2, 'Physics'), (2, 'WALENIE KONIA');
 
 INSERT INTO student_subject (student_id, subject_id) VALUES (1, 3);
 
-INSERT INTO grade (id,subject_id, student_id, teacher_id, numerical_value, description)
-VALUES (1,3,1,1,4,'siemanko');
+-- INSERT INTO grade (id,subject_id, student_id, teacher_id, numerical_value, description)
+-- VALUES (1,3,1,1,4,'siemanko');
 
 
 
