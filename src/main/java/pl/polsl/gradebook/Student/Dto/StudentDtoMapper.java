c82d63b800716e1fdbaa64d1c63a9cd11a355eeb
Dominik Szczepanik <dominik16333@gmail.com>
2023-06-20 20:38:29 +0200

    creating student dto and mapper

---
+++ src/main/java/pl/polsl/gradebook/Student/Dto/StudentDtoMapper.java
@@ -0,0 +1,35 @@
+package pl.polsl.gradebook.Student.Dto;
+
+import org.springframework.stereotype.Service;
+import pl.polsl.gradebook.Student.Model.Student;
+import pl.polsl.gradebook.Teacher.Dto.TeacherRegisterDto;
+import pl.polsl.gradebook.Teacher.Model.Teacher;
+import pl.polsl.gradebook.User.Model.User;
+
+@Service
+public class StudentDtoMapper {
+
+
+    public static User mapDtoToUser(StudentRegisterDto dto) {
+        User user = new User();
+
+        user.setLogin(dto.getLogin());
+        user.setPassword(dto.getPassword());
+        user.setRole("STUDENT");
+
+        return user;
+    }
+
+    public static Student mapDtoToStudent(StudentRegisterDto dto) {
+        Student student = new Student();
+        student.setName(dto.getName());
+        student.setSurname(dto.getSurname());
+        // change this in the future
+        student.setParent_number("666666666");
+
+        return student;
+    }
+
+
+
+}
---
+++ src/main/java/pl/polsl/gradebook/Student/Dto/StudentRegisterDto.java
@@ -0,0 +1,31 @@
+package pl.polsl.gradebook.Student.Dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class StudentRegisterDto {
+
+    @NotBlank(message = "Imie nie może pozostać puste !")
+    private String name;
+
+    @NotBlank(message = "Nazwisko nie może pozostać puste")
+    private String surname;
+
+    // add validation to this
+    private String parent_number;
+
+    @NotBlank(message = "Pole nie może być puste!")
+    private String login;
+
+    @NotBlank(message = "Pole nie może być puste!")
+    private String password;
+
+
+}
