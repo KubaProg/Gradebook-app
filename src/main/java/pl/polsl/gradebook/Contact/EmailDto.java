5f0c9eb73e306f78d1b3c1ddab5ea2e4091d85f0
Jakub Opiełka <jakub1.opielka1@interia.pl>
2023-06-20 19:27:55 +0200

    Working on the sending message on landing-page started

--- src/main/java/pl/polsl/gradebook/Contact/EmailDto.java
+++ src/main/java/pl/polsl/gradebook/Contact/EmailDto.java
@@ -1,10 +1,24 @@
 package pl.polsl.gradebook.Contact;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class EmailDto {
 
     private String fromEmail;
 
     private String toEmail;
+
+    private String subject;
+
+    private String body;
+
 
 
 }
--- src/main/java/pl/polsl/gradebook/Web/HomeController.java
+++ src/main/java/pl/polsl/gradebook/Web/HomeController.java
@@ -1,8 +1,19 @@
 package pl.polsl.gradebook.Web;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import pl.polsl.gradebook.Contact.EmailDto;
+import pl.polsl.gradebook.Teacher.Dto.TeacherDtoMapper;
+import pl.polsl.gradebook.Teacher.Dto.TeacherRegisterDto;
+import pl.polsl.gradebook.Teacher.Model.Teacher;
+import pl.polsl.gradebook.User.Model.User;
 
 @Controller
 @RequestMapping("/")
@@ -12,6 +23,20 @@ public class HomeController {
     public String hello(){
         return "landing-page";
     }
+
+    @ModelAttribute("emailDto")
+    public EmailDto getMessageInfo(){
+        return new EmailDto();
+    }
+    @PostMapping("/send-mail")
+    public String sendMail(@Valid EmailDto emailDto, Errors errors)
+    {
+
+
+        return "redirect:/";
+    }
+
+
 
 
 }
